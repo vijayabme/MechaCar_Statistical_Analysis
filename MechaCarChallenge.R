f3c0d7b3feb8356afffd80f9d6a87a2230d538eb
Vijaya Ayanampudi <ayanampudi.v@gmail.com>
2021-09-06 11:11:01 -0700

    Updated T-tests code and results

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,5 +6,18 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 library(tidyverse)
 
 susp_coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
-total_summary <- susp_coil %>% summarize(Mean=mean(PSI),Median = median(PSI),SD = sd(PSI), ) #create summary table
-lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),SD = sd(PSI), .groups = 'keep') #create summary table
+total_summary <- susp_coil %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), ) #create summary table
+lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') #create summary table
+
+# T-test 
+
+t.test(log10(sample_table$Miles_Driven),mu=mean(log10(population_table$Miles_Driven))) #compare sample versus population means
+
+lot1 <- subset(susp_coil, Manufacturing_Lot == "Lot1") # filter Lot1 data
+lot2 <- subset(susp_coil, Manufacturing_Lot == "Lot2") # filter Lot1 data
+lot3 <- subset(susp_coil, Manufacturing_Lot == "Lot3") # filter Lot1 data
+
+t.test(log10(lot1$PSI),mu=mean(log10(susp_coil$PSI)))
+t.test(log10(lot2$PSI),mu=mean(log10(susp_coil$PSI)))
+t.test(log10(lot3$PSI),mu=mean(log10(susp_coil$PSI)))
+t.test(log10(susp_coil$PSI),mu=mean(log10(susp_coil$PSI)))
--- README.md
+++ README.md
@@ -21,7 +21,14 @@ The linear model is reasonably predicting the mpg of Mechacar prototypes. But it
 We can try and create more simple and multiple linear regression models to create more accurate predictive models.
 
 ## Summary Statistics on Suspension Coils
+The design specifications dictate that the variance for the suspension coils must not exceed 100 PSI. The total summary across all suspension coil lots indicate a variance of 62.3 which is below the design specification.
+But if we look at individual lots, the Lot 3 variance is higher than 100 PSI.The Lot 3 doesnt meet the design specification for the PSI.  
 
 
 ## T-Tests on Suspension Coils
 
+If p-value is not significant then we dont have sufficient evidence to reject the null hypothesis, and the two means are statistically similar.
+
+- The PSI across all manufacturing lots is statistically similar to the population mean as thge p-value is not statistically significant
+- The Lot1 and Lot2 p-values are significant, which indicates we have sufficient evidence to reject the null hypothesis. So the Lot1 mean and Lot2 mean are statistically different from the population mean.
+- The Lot3 p-value is not statistically significant, so we can say that the Lot3 mean is statistically similar to the population mean. 
